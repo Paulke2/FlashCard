ae342693ff35d2cf373b30bd12256aa26093b051
Paulke2 <paulke@udel.edu>
2023-01-03 18:38:40 -0500

    created output file method

--- CreateDeck.java
+++ CreateDeck.java
@@ -1,5 +1,8 @@
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
 public class CreateDeck{
     static Scanner scr = new Scanner(System.in);
     ArrayList<Card> deck = new ArrayList<>();
@@ -15,4 +18,36 @@ public class CreateDeck{
         Card newCard= new Card(question,answer,cardNum);
         deck.add(newCard);
     }
+    public static void PrintDeck(ArrayList<Card> deck){
+        int temp=cardNum;
+        while(temp-1>0) {
+            Card currCard= deck.get(temp - 1);
+            System.out.println("---------------------------------------------");
+            System.out.println("question: ");
+            System.out.println(currCard.getQuestion());
+            System.out.println("||||||||||||||||||||||||||||||||||");
+            System.out.println("answer: ");
+            System.out.println(currCard.getAnswer());
+            System.out.println("---------------------------------------------");
+            temp--;
+        }
+    }
+    public static void CreateSheet(ArrayList<Card> deck) throws IOException {
+        System.out.println("creating a study guide. What should the file name be?");
+        String fileName = scr.nextLine();
+        fileName = fileName + ".txt";
+        File studyGuide = new File(fileName);
+        //now writing to file.
+        FileWriter cardAdder = new FileWriter(fileName);
+
+        int temp = cardNum;
+        while (temp - 1 > 0) {
+            Card currCard = deck.get(temp - 1);
+        cardAdder.write(temp+(")")+"\n");
+            cardAdder.write("Q. "+currCard.getQuestion());
+            cardAdder.write("A. "+currCard.getAnswer());
+        temp--;
+        }
+    }
+
 }
