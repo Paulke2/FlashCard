cd726657fdd86eb0f99c66252696e65c1d73a076
Paulke2 <paulke@udel.edu>
2023-01-12 14:41:18 -0500

    fixed comp. of strings

--- Control.java
+++ Control.java
@@ -1,6 +1,7 @@
-import java.util.Scanner;
-import java.util.ArrayList;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Control {
     static Scanner scr = new Scanner(System.in);
 
@@ -9,41 +10,45 @@ public class Control {
     }
     public String getNextAction(){
         /**
-         * this method gets users next action
+         * this method gets users next action.
          */
         System.out.println("enter \"n\" to make a new card, \"u\" to update an existing card, \"p\" to print the current list of cards, and \"s\" to create a study sheet from the current cards.");
-   String input = scr.nextLine();
-   while(!(input=="n"||input=="p"||input=="s"||input=="u")){
+        String input = scr.next();
+        while(!(input.equals("n")||input.equals("p")||input.equals("s")||input.equals("u"))){
 
-       System.out.println("please give a valid input.");
-       System.out.println("again the options are \"n\" to make a new card, \"u\" to update an existing card, \"p\" to print the current list of cards, and \"s\" to create a study sheet from the current cards.");
-       input =scr.nextLine();
-   }
 
-   return input;
+            System.out.println("please give a valid input. you said "+input);
+            System.out.println("again the options are \"n\" to make a new card, \"u\" to update an existing card, \"p\" to print the current list of cards, and \"s\" to create a study sheet from the current cards.");
+            input =scr.nextLine();
+
+            System.out.println("test "+input);
+
+        }
+        System.out.println(input);
+        return input;
     }
     public void Continue() throws IOException {
         /**this method gets input from get next action and if its n,(new card) it will continue until the question matches u,p, or s
-        **/
+         **/
         ArrayList<Card> deck=new ArrayList<Card>();
         Deck newDeck = new Deck(deck,0);
         String input = getNextAction();
         String question;
         String answer;
-
-        if (input =="n"){
+        System.out.println("got here");
+        if (input.equals("n")){
             boolean add=true;
             while(add==true){
                 add=newDeck.AddCard();
             }
         }
-        if(input=="p"){
+        if(input.equals("p")){
             newDeck.PrintDeck();
         }
-        if(input=="s"){
+        if(input.equals("s")){
             newDeck.CreateSheet();
         }
-        if(input=="u"){
+        if(input.equals("u")){
             newDeck.updateCard();
         }
     }
