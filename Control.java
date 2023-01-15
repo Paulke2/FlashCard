934917405ad36c9896057d8f10c76275791cd719
Paulke2 <paulke@udel.edu>
2023-01-14 20:38:34 -0500

    control finished. just need to polish Create sheet method in deck. Also hashing cards in update cards will be new goal

--- Control.java
+++ Control.java
@@ -24,41 +24,77 @@ public class Control {
             System.out.println("test "+input);
 
         }
-        System.out.println(input);
+
         return input;
     }
     public void Continue() throws IOException {
         /**this method gets input from get next action and if its n,(new card) it will continue until the question matches u,p, or s
          **/
+        boolean determine_End=true;
         ArrayList<Card> deck=new ArrayList<Card>();
         Deck newDeck = new Deck(deck,0);
-        String input = getNextAction();
+        //String input = getNextAction();
         String question;
         String answer;
+        //this loops through until users satisfied with thier set
+        while(determine_End) {
+            String input = getNextAction();
+            if (input.equals("n")) {
+                boolean add = true;
+                while (add == true) {
+                    add = newDeck.AddCard();
+                    if(add==true) {
+                        newDeck.cardNum++;
+                    }
+                    newDeck.PrintDeck();
 
-        if (input.equals("n")){
-            boolean add=true;
-            while(add==true){
-                add=newDeck.AddCard();
-                newDeck.cardNum++;
+                }
+                //test issue of when add is false.
+
+
+            }
+            if (input.equals("p")) {
                 newDeck.PrintDeck();
-                System.out.println("should print cards");
+            }
+            if (input.equals("s")) {
+                newDeck.CreateSheet();
+            }
+            if (input.equals("u")) {
+                newDeck.updateCard();
+            }
+
+            determine_End=End();
+            if(determine_End==false){
+                //break out of loop only if user says they want to stop working with curr set.
+                break;
             }
         }
-        if(input.equals("p")){
-            newDeck.PrintDeck();
+        newDeck.CreateSheet();
+
+    }
+    public static boolean End(){
+        /**
+         * this is a helper function for continue to determine if user wants to continue with curr cards or end. returns false to end and true to continue
+         */
+        String input ="";
+        System.out.println("would you like to end the set or continue working with this set? press \"c\" to continue and \"e\" to end.");
+        input = scr.next();
+        while(!(input.equals("e")||input.equals("c"))){
+            System.out.println("again, press \"c\" to continue and \"e\"to end.");
+            input = scr.next();
         }
-        if(input.equals("s")){
-            newDeck.CreateSheet();
+
+        if(input.equals("e")){
+            return false;
         }
-        if(input.equals("u")){
-            newDeck.updateCard();
-        }
+        return true;
+
     }
     public static void main(String[] args) throws IOException {
         System.out.println("Starting");
         Control controller = new Control();
         controller.Continue();
+        System.out.println("-----------------------Final Deck-----------------------");
 
 
     }
